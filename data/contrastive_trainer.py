cbf590c95a4ec5573e300b673048235e373601fb
jp1924 <jp42maru@gmail.com>
2024-03-14 05:50:12 +0000

    Add: contrastive_trainer

---
+++ data/contrastive_trainer.py
@@ -0,0 +1,20 @@
+from typing import Optional, Any
+
+from datasets import Dataset
+from torch.utils.data import Dataset, Sampler
+from transformers import Trainer
+from .weighted_sampler import WeightedSampler
+
+
+class ContrastiveTrainer(Trainer):
+    def _get_train_sampler(self) -> Optional[Sampler]:
+        return WeightedSampler(
+            batch_size=self.args.train_batch_size,
+            dataset=self.train_dataset,
+        )
+
+    def _get_eval_sampler(self, eval_dataset: Dataset) -> Any | None:
+        return WeightedSampler(
+            batch_size=self.args.per_device_eval_batch_size,
+            dataset=self.eval_dataset,
+        )
